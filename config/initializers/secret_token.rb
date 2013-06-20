51f40c4d7fdc6524f3f9fd3ce2f7570501196e29
David Hope <dmhope1106@gmail.com>
2013-06-20 17:35:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TennisLadder::Application.config.secret_token = '639b2f8a114fece1af7b60d8b6168a921b53f03e64594ddfa00c4f0f90dae13091b6f34686727a0f48a2bdc9cbd385cb9221e12e023ded6b2747d05858433de4'
